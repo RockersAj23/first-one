d9202e2856fc56865046793ae6b1f01edd61949e
RockersAj23 <mulgir.ajay.che22@iitbhu.ac.in>
2023-09-17 18:17:22 +0530

    i created one file based on loops

---
+++ loop.py
@@ -0,0 +1,8 @@
+# Example 1: Iterate over a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Example 2: Iterate over a range of numbers
+for i in range(1, 6):
+    print(i)
