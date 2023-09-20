6f65744d35e8d97b15eb5792885147aa0af98ca7
RockersAj23 <mulgir.ajay.che22@iitbhu.ac.in>
2023-09-20 10:52:59 +0530

    jupiter notebook also added

--- practice.py
+++ practice.py
@@ -1,3 +1,20 @@
-a = [3,4,5]
-b=[6,7,8]
-print(a+b)
+n_string = input()
+
+n = n_string[0]
+k=  n_string[2]
+arr_n = []
+n_string2 = input()
+element = n_string2.split()
+for i in element:
+    arr_n.append(int(i))
+
+ref = arr_n[int(k)-1]
+answer =0
+
+for i in arr_n:
+    if i<ref:
+        break
+    if i!=0:
+        answer = answer +1
+
+print(answer)
