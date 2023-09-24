d598db16faa7a72414f75a232780d1bcc1571dcb
RockersAj23 <mulgir.ajay.che22@iitbhu.ac.in>
2023-09-24 19:49:09 +0530

    first ML model

--- practice.cpp
+++ practice.cpp
@@ -1,9 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a;
-    int b;
-    cin>>a>>b;
-    cout<<a+b;
+int main()
+{
+    int arr[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int ones = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (arr[i] == 1)
+        {
+            ones++;
+        }
+    }
+    int a = 0;
+    int b = arr[1];
+    int c = 0;
+    if (arr[0] <= arr[2])
+    {
+        a = arr[0];
+        c = arr[2];
+    }
+    else
+    {
+        a = arr[2];
+        c = arr[0];
+    }
+    if (ones == 3 )
+    {
+        cout << a + b + c;
+    }
+    else if (ones == 0)
+    {
+        cout << a * b * c;
+    }else if (ones == 2 && b != 1)
+    {
+        cout << a + b + c;
+    }else{
+        cout<<(a+b)*c;
+    }
+    
+    
+    
     return 0;
 }
--- practice.py
+++ practice.py
@@ -1,20 +1,10 @@
-n_string = input()
 
-n = n_string[0]
-k=  n_string[2]
-arr_n = []
-n_string2 = input()
-element = n_string2.split()
-for i in element:
-    arr_n.append(int(i))
+"""a = input()
+b=input()
+try:
+    print("sum=",
+          int(a)+int(b))
+except Exception as e:
+    print("error")
 
-ref = arr_n[int(k)-1]
-answer =0
-
-for i in arr_n:
-    if i<ref:
-        break
-    if i!=0:
-        answer = answer +1
-
-print(answer)
+print("we learned exception handling")"""
